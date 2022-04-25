27b237d198bc088f88f69dfe472181a3471113f8
yanyuhao <2499787198@qq.com>
2022-04-25 09:17:37 +0800

    缓存优化处理（aop）

--- blog-api/src/main/java/com/yyh/blog/controller/ArticleController.java
+++ blog-api/src/main/java/com/yyh/blog/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.yyh.blog.controller;
 
 import com.yyh.blog.common.aop.LogAnnotation;
+import com.yyh.blog.common.cache.Cache;
 import com.yyh.blog.service.ArticleService;
 import com.yyh.blog.vo.Result;
 import com.yyh.blog.vo.params.ArticleParam;
@@ -20,7 +21,8 @@ public class ArticleController {
      */
     @PostMapping
     // 加上此注解 代表要对此接口记录日志
-        @LogAnnotation(module="文章",operator="获取文章列表")
+    @LogAnnotation(module = "文章", operator = "获取文章列表")
+    @Cache(expire = 5*60*1000,name = "listArticle")
     public Result listArticle(@RequestBody PageParams pageParams) {
         return articleService.listArticle(pageParams);
     }
@@ -29,6 +31,8 @@ public class ArticleController {
      * 首页 最热文章
      */
     @PostMapping("hot")
+    // 使切面可以拿到 ArticleController 和 hotArticle
+    @Cache(expire = 5 * 60 * 1000, name = "hot_article")
     public Result hotArticle() {
         int limit = 5;
         return articleService.hotArticle(limit);
@@ -38,6 +42,7 @@ public class ArticleController {
      * 首页 最热文章
      */
     @PostMapping("new")
+    @Cache(expire = 5 * 60 * 1000, name = "news_article")
     public Result newArticle() {
         int limit = 5;
         return articleService.newArticle(limit);
@@ -54,11 +59,12 @@ public class ArticleController {
 
     /**
      * 根据id 查询文章
+     *
      * @param articleId
      * @return
      */
     @PostMapping("view/{id}")
-    public Result findArticleById(@PathVariable("id") Long articleId){
+    public Result findArticleById(@PathVariable("id") Long articleId) {
         return articleService.findArticleById(articleId);
     }
 
@@ -66,7 +72,7 @@ public class ArticleController {
      * 文章发布服务
      */
     @PostMapping("publish")
-    public Result publish(@RequestBody ArticleParam articleParam){
+    public Result publish(@RequestBody ArticleParam articleParam) {
         return articleService.publish(articleParam);
     }
 
